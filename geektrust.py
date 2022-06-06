28bdbb7c24a71642551b42d39c7b1e1282965389
rjrajivjha <rajivjha1996@gmail.com>
2022-06-06 16:26:38 +0530

    refactor main function

--- geektrust.py
+++ geektrust.py
@@ -15,33 +15,35 @@ def process_Lines(Lines):
 
         if command == START_SUBSCRIPTION:
             subscription_start_date = validate_date(args[0])
+            continue
 
-        if subscription_start_date is None and command == ADD_SUBSCRIPTION:
+        elif subscription_start_date is None and command == ADD_SUBSCRIPTION:
             print(f'{ADD_SUBSCRIPTION_FAILED} {INVALID_DATE}')
 
-        if subscription_start_date is None and command == ADD_TOP_UP:
+        elif subscription_start_date is None and command == ADD_TOP_UP:
             print(f'{ADD_TOP_UP_FAILED} {INVALID_DATE}')
 
-        if command == ADD_SUBSCRIPTION:
+        elif command == ADD_SUBSCRIPTION:
             category = args[0]
             plan = args[1]
             add_sub(category, plan, subscriptions, subscription_start_date)
 
-        if command == ADD_TOP_UP and subscription_start_date and top_ups:
+        elif command == ADD_TOP_UP and subscription_start_date and top_ups:
             print("ADD_TOPUP_FAILED DUPLICATE_TOPUP")
 
-        if command == ADD_TOP_UP and subscription_start_date and not top_ups:
+        elif command == ADD_TOP_UP and subscription_start_date and not top_ups:
             top_ups += add_top_up(top_ups_dict[args[0]], int(args[1]), subscriptions)
 
-        if command == PRINT_RENEWAL_DETAILS:
+        elif command == PRINT_RENEWAL_DETAILS:
             print_renewal_details(subscriptions, top_ups)
 
 
 def main():
-    if len(argv) != ARGUMENTS_LENGTH:
-        raise Exception("File path not entered")
+    # if len(argv) != ARGUMENTS_LENGTH:
+    #     raise Exception("File path not entered")
     try:
-        file_path = argv[1]
+        # file_path = argv[1]
+        file_path = 'sample_input/input8.txt'
         f = open(file_path, 'r')
         Lines = f.readlines()
         process_Lines(Lines)
